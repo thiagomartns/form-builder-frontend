b1d30d1ea0df893fe7f7e7284be827b4ceeaf22a
Thiago <tmg3.mn@gmail.com>
2025-11-03 12:48:25 -0300

    refact: remove chamadas a sortable

--- src/components/form-content/blocks/block.tsx
+++ src/components/form-content/blocks/block.tsx
@@ -1,10 +1,8 @@
 import useFieldActions from "@/hooks/useFieldActions";
-import { useSortable } from "@dnd-kit/sortable";
 import { ActionIcon } from "@mantine/core";
 import { useFocusWithin } from "@mantine/hooks";
 import { Grip, Trash } from "lucide-react";
 import React from "react";
-import { CSS } from "@dnd-kit/utilities";
 
 interface BlockProps {
   children: React.ReactNode;
@@ -16,29 +14,15 @@ export default function Block({ children, id }: BlockProps) {
 
   const { removeField, selectField, selectedField } = useFieldActions();
 
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({
-      id,
-    });
-
   const handleClick = () => {
     selectField(id);
   };
 
   const isFieldSelected = selectedField === id;
 
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-  };
-
   return (
     <div
       onClick={handleClick}
-      ref={setNodeRef}
-      style={style}
-      {...listeners}
-      {...attributes}
       className={`relative pb-2 mr-8  border-b-neutral-200 transition-all cursor-pointer ${
         isFieldSelected
           ? "border-blue-400 bg-blue-50/40"
@@ -59,8 +43,6 @@ export default function Block({ children, id }: BlockProps) {
           </ActionIcon>
           <ActionIcon
             size="xl"
-            {...listeners}
-            {...attributes}
             className="bg-green-100 rounded-full text-green-600 hover:bg-green-200 cursor-grab active:cursor-grabbing"
           >
             <Grip />
--- src/components/form-content/index.tsx
+++ src/components/form-content/index.tsx
@@ -7,10 +7,6 @@ import { NameBlock } from "./blocks/name-block";
 import { EmailBlock } from "./blocks/email-block";
 import { PhoneBlock } from "./blocks/phone-block";
 import useFieldActions from "@/hooks/useFieldActions";
-import {
-  SortableContext,
-  verticalListSortingStrategy,
-} from "@dnd-kit/sortable";
 
 export default function FormContent() {
   const { setNodeRef, isOver } = useDroppable({
@@ -23,38 +19,34 @@ export default function FormContent() {
     <Card className="shadow-2xl ">
       <Card.Section ref={setNodeRef} className={`p-4 px-12 min-h-[400px]}`}>
         {fields.length === 0 && (
-          <div className="text-center text-gray-400">
+          <div className="text-center text-gray-400 min-h-[200px] flex items-center justify-center">
             {isOver
               ? "Solte aqui para adicionar o campo"
               : "Arraste campos da sidebar para começar"}
           </div>
         )}
-        <SortableContext
-          items={fields.map((field) => field.id)}
-          strategy={verticalListSortingStrategy}
-        >
-          <div>
-            {fields.map((field) => {
-              if (field.type === "title") {
-                return <TitleBlock key={field.id} id={field.id} />;
-              }
 
-              if (field.type === "name") {
-                return <NameBlock key={field.id} id={field.id} />;
-              }
+        <div>
+          {fields.map((field) => {
+            if (field.type === "title") {
+              return <TitleBlock key={field.id} id={field.id} />;
+            }
 
-              if (field.type === "email") {
-                return <EmailBlock key={field.id} id={field.id} />;
-              }
+            if (field.type === "name") {
+              return <NameBlock key={field.id} id={field.id} />;
+            }
 
-              if (field.type === "telephone") {
-                return <PhoneBlock key={field.id} id={field.id} />;
-              }
+            if (field.type === "email") {
+              return <EmailBlock key={field.id} id={field.id} />;
+            }
 
-              return <h1 key={field.id}>{field.label}</h1>;
-            })}
-          </div>
-        </SortableContext>
+            if (field.type === "telephone") {
+              return <PhoneBlock key={field.id} id={field.id} />;
+            }
+
+            return <h1 key={field.id}>{field.label}</h1>;
+          })}
+        </div>
       </Card.Section>
     </Card>
   );
